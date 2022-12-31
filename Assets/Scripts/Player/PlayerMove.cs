a50dc30e21e5cc2d23217193f0237ab681037857
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2022-12-31 15:23:47 +0100

    Test-Animations for the Player

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,14 +6,59 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] Rigidbody2D self;
-    Vector2 move;
+    [SerializeField] Animator playerAnimator;
+    Vector2 directions;     
 
-    // Update is called once per frame
     void Update()
     {
-        move.x = Input.GetAxis("Horizontal") * moveSpeed;
-        move.y = Input.GetAxis("Vertical") * moveSpeed;
+        directions.x = Input.GetAxis("Horizontal");
+        directions.y = Input.GetAxis("Vertical");
 
-        self.velocity = move;
+        FacingDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 move;
+        move.x = directions.x * moveSpeed;
+        move.y = directions.y * moveSpeed;        
+        self.velocity = move;        
+    }
+
+    private void FacingDirection()
+    {
+        // nothing
+        if (directions.y != 0 || directions.x != 0)
+        {
+            // facing left
+            if (directions.x < 0)
+            {
+                playerAnimator.SetFloat("Vertical", -1);
+                playerAnimator.SetFloat("Horizontal", 0);
+            }
+            // facing right
+            else if (directions.x > 0)
+            {
+                playerAnimator.SetFloat("Vertical", 1);
+                playerAnimator.SetFloat("Horizontal", 0);
+            }
+            // facing up
+            else if (directions.y > 0)
+            {
+                playerAnimator.SetFloat("Horizontal", 1);
+                playerAnimator.SetFloat("Vertical", 0);
+            }
+            // facing down
+            else if (directions.y < 0)
+            {
+                playerAnimator.SetFloat("Horizontal", -1);
+                playerAnimator.SetFloat("Vertical", 0);
+            }
+        }
     }
 }
