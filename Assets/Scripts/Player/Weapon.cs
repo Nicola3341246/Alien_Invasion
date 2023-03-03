cebdd4f2928edbce96a502b9b5b97220c048c32a
Ahmad Becirovic <Crunch2404@hotmail.com>
2023-03-03 09:07:59 +0100

    Weapon Rotate

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -13,7 +13,7 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        WeaponRotate();
     }
 
     private void WeaponRotate()
@@ -21,7 +21,11 @@ public class Weapon : MonoBehaviour
         //kopiert vom Internet https://answers.unity.com/questions/855976/make-a-player-model-rotate-towards-mouse-location.html
         Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
         Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        float angle = AngleBetweenPoints(positionOnScreen, mouseOnScreen);
         transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+    private static float AngleBetweenPoints(Vector2 a, Vector2 b) 
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg; 
     }
 }
